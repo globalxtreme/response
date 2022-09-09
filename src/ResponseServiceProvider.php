2ae5401fafb5d3c345fc25500b5867431d9c1034
yuswa-arba <yuswa.bitcoin98@gmail.com>
2022-09-10 03:54:00 +0800

    TASK: Add configuration

--- src/ResponseServiceProvider.php
+++ src/ResponseServiceProvider.php
@@ -15,10 +15,14 @@ class ResponseServiceProvider extends ServiceProvider
             ]);
 
         }
+
+        $this->publishes([
+            __DIR__.'/../config/http-status.php' => config_path('http-status.php'),
+        ],'response-config');
     }
 
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../config/http-status.php', 'http-status');
     }
 }
